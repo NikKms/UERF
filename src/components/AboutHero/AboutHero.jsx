682d72cdb66a6622d5a21f1d3a40e5af746f9922
LosOleksandr <losalexander2003@gmail.com>
2023-09-29 18:30:14 +0300

    add animations and responsive design on about page

--- src/components/AboutHero/AboutHero.jsx
+++ src/components/AboutHero/AboutHero.jsx
@@ -1,11 +1,15 @@
 // import React from 'react';
 import bgImg from '../../assets/img/about_hero.jpg';
 
-import { Center, Heading, Text } from '@chakra-ui/react';
+import { Center, Text } from '@chakra-ui/react';
+import WaveText from './WaveText';
+import { useTranslation } from 'react-i18next';
 
 const AboutHero = () => {
   const linearGradient = `linear-gradient(to top, rgba(6, 7, 7, 0.8),rgba(34, 32, 32, 0.4))`;
   const containerStyle = `${linearGradient}, url(${bgImg}) no-repeat center`;
+
+  const { t } = useTranslation();
 
   return (
     <Center
@@ -15,11 +19,23 @@ const AboutHero = () => {
       height={'600px'}
       flexDirection={'column'}
     >
-      <Heading as={'h1'} color={'whiteAlpha.900'} size={'2xl'}>
-        About
-      </Heading>
-      <Text fontSize={'xl'} marginTop={'12px'} color={'whiteAlpha.900'}>
-        😎 About
+      <WaveText
+        style={{
+          fontSize: '3rem',
+          fontWeight: '500',
+          color: '#fff',
+          textTransform: 'uppercase',
+        }}
+      >
+        {t('aboutPage.hero.title')}
+      </WaveText>
+      <Text
+        fontSize={'xl'}
+        marginTop={'12px'}
+        textAlign={'center'}
+        color={'whiteAlpha.900'}
+      >
+        Lorem ipsum dolor
       </Text>
     </Center>
   );
