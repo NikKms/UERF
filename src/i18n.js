e16c8771acf05002f829207019e0857efa337a5e
NikKms <mykola.kamyshnikov@gmail.com>
2023-09-29 13:51:00 +0300

    change btn

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,12 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+
+
+
+
+
+
 i18n
     .use(Backend)
     .use(LanguageDetector)
