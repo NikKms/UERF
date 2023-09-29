037e9691819c1b714ccbc8c2c167102fb61f1d8e
NikKms <mykola.kamyshnikov@gmail.com>
2023-09-29 17:01:39 +0300

    footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,24 +1,47 @@
-import { Button, useDisclosure } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import React from 'react';
-import Btn from '../Btn/Btn';
-import { ModalContact } from '../ModalContact/ModalContact';
-import { ContactForm } from '../ContactForm/ContactForm';
+import bg from '../../assets/img/footer-bg.jpg'
+import FooterAbout from '../FooterAbout/FooterAbout';
+import FooterGallery from '../FooterGallery/FooterGallery';
+import LangSelect from '../LangSelect/LangSelect';
 
 const Footer = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleModal = () => {
-    onOpen();
-    console.log(isOpen);
-  };
-
   return (
-    <div>
-      Footer <Button onClick={handleModal}>Modal</Button>
-      <ModalContact isOpen={isOpen} onClose={onClose}>
-        <ContactForm />
-      </ModalContact>
-    </div>
+    <Box as='footer'
+      width='full'
+      minH='150px'
+      bgColor='teal'
+      position="relative"
+      backgroundImage={`url(${bg})`}
+      bgPos="center"
+      bgSize='cover'
+      bgRepeat="no-repeat"
+    >
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        bg="rgba(0, 0, 0, 0.3)"
+      />
+      <Container
+        display='flex'
+        flexDirection={{ base: 'column', md: 'row' }}
+        alignItems='center'
+        justifyContent='space-between'
+        maxW={{ base: '744px', lg: '1000px', xl: '1176px' }}
+        px="12px"
+        py='24px'
+        position="relative"
+        zIndex="1"
+        mx="auto"
+        gap='32px'
+      >
+        <FooterAbout />
+        <FooterGallery />
+      </Container>
+    </Box>
   );
 };
 
