d05809175876085239d4f908ecdb3edbce8e6392
Elvira Lemak <elvralema@rambler.ru>
2023-10-03 11:42:31 +0200

    fix path images

--- src/components/EventInfo/EventSlider.jsx
+++ src/components/EventInfo/EventSlider.jsx
@@ -1,16 +1,39 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import PropTypes from 'prop-types';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
 import './slider.css';
-
 import { Pagination, Navigation } from 'swiper/modules';
 import { Box, Container } from '@chakra-ui/react';
+import event1_1 from '../../assets/img/event1-1.jpg';
+import event1_2 from '../../assets/img/event1-2.jpg';
+import event1_3 from '../../assets/img/event1-3.jpg';
+import event1_4 from '../../assets/img/event1-4.jpg';
+import event1_5 from '../../assets/img/event1-5.jpg';
+import event1_6 from '../../assets/img/event1-6.jpg';
+import event1_7 from '../../assets/img/event1-7.jpg';
+import { useParams } from 'react-router-dom';
 
-const EventSlider = ({ data }) => {
+const images = [
+  { id: '01', imgArr: [] },
+  {
+    id: '02',
+    imgArr: [
+      event1_1,
+      event1_2,
+      event1_3,
+      event1_4,
+      event1_5,
+      event1_6,
+      event1_7,
+    ],
+  },
+];
+
+const EventSlider = () => {
+  const { id } = useParams();
+  const selectedImages = images.find(img => img.id === id);
+
   return (
     <Box
       as="section"
@@ -45,8 +68,8 @@ const EventSlider = ({ data }) => {
           }}
           modules={[Pagination, Navigation]}
         >
-          {data.imagesSlider &&
-            data.imagesSlider.map((image, index) => (
+          {selectedImages.imgArr.length > 0 &&
+            selectedImages.imgArr.map((image, index) => (
               <SwiperSlide key={index}>
                 <img src={image} alt={`Slide ${index + 1}`} />
               </SwiperSlide>
@@ -55,10 +78,6 @@ const EventSlider = ({ data }) => {
       </Container>
     </Box>
   );
-};
-
-EventSlider.propTypes = {
-  data: PropTypes.object,
 };
 
 export default EventSlider;
